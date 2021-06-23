16f43f8b8da36d5dd394f4ed7a4fa22514c5e9e8
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-06-23 16:09:10 -0300

    adicionado exercicio 2 bloco 4.1

---
+++ exercicios/4.1 introducao JS/4.1exercicio2.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 5;
+
+if (a > b) {
+    console.log("número maior é", a);
+}
+else if (a < b) {
+    console.log("número maior é", b);
+} else {
+    console.log("números identicos o número é", a);
+}
