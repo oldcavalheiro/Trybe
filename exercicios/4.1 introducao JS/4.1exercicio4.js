66b99f14024b48d619e5c1e2ceefac92b7535d7a
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-06-23 18:08:17 -0300

    add 4.1exercicio4.js

---
+++ exercicios/4.1 introducao JS/4.1exercicio4.js
@@ -0,0 +1,16 @@
+/* dado um valor definido numa variável, 
+retorne "positive" se esse valor for positivo,
+        "negative" se for negativo e
+        "zero" caso contrário. */
+
+let i = -3;
+
+if (i > 0) {
+    console.log("positeve")
+}
+else if (i < 0) {
+    console.log("negative")
+}
+else {
+    console.log("zero")
+}
