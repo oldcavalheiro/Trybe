d871c2aa4e3adb94ff25d4adc3e1427b4c1daa95
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-07-07 14:47:15 -0300

    adicionado exercicio do calendario

---
+++ exercicios/5.3 DOM events/Calendar/Calendar.js
@@ -0,0 +1,27 @@
+// criando dias da semana
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+
+// 1. criando dias do mês
+let daysArray = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth() {
+  for (let index = 0; index < daysArray.length; index += 1){
+    console.log(daysArray[index]);
+  }
+
+}
+
+createDaysOfTheMonth();
