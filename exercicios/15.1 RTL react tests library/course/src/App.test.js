b9ab91dbfccbc28df2bf287d900466767f4041ce
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-09-24 18:57:52 -0300

    add RTL introduction

---
+++ exercicios/15.1 RTL react tests library/course/src/App.test.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// seletores - acessar os elementos da sua tela
+// eventos   - com eles (se houver necessidade)
+// matchers  - seu teste
+
+/* seletores:
+getByLabelText()
+getAllByRole() // 'button'
+getByTestId()
+*/
+
+// describe('', () => { });
+// test alias it
+
+// input test with label text
+test('Verificando se existe o campo Email.', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+/*/ only for 1 button
+test('Verificando se existe um botão', () => {
+  const { getByRole } = render(<App />);
+  const btn = getByRole('button');
+  expect(btn).toBeInTheDocument();
+}); */
+
+// test for 2x buttons
+test('Verificando se existe dois botões', () => {
+  const { getAllByRole } = render(<App />);
+  const buttons = getAllByRole('button');
+  expect(buttons.length).toBe(2);
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  const { getByTestId } = render(<App />);
+  const btn = getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+  expect(btn.type).toBe('button');
+  expect(btn).toHaveValue('Enviar');
+});
+
+// teste de evento (fireEvent)
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  const { getByTestId, getByLabelText } = render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = getByTestId('id-send');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+  fireEvent.click(btnSend);
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+});
