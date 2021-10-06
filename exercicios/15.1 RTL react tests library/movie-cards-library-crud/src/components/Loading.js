c03566c8a3b3f9a8bf0c1dd75117de61175d910d
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-10-06 13:52:38 -0300

    add arquivos RTL

--- exercicios/15.1 RTL react tests library/movie-cards-library-crud/src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class Loading extends Component {
-  render() {
-    return <div className="loading">Carregando...</div>;
-  }
-}
-
-export default Loading;
---
+++ exercicios/15.3 RTL Router/aula-ao-vivo/portfolio_example/src/tests/renderWithRouter.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+
+function renderWithRouter(component) {
+    const customHistory = createMemoryHistory()
+    const selectorsRTL = render(
+      <Router history={customHistory} >
+        { component }
+      </Router>
+    );
+
+    return {
+        ...selectorsRTL,
+        history: customHistory,
+    }
+}
+
+export default renderWithRouter;
+
+////////////
+// renderWithRouter(<QualquerComponent />)
+
